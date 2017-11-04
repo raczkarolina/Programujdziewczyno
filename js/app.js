9afa5f061533f8ee0abf389da5fa967520f4cc9f
raczkarolina <kkarolina99@gmail.com>
2017-11-04 17:07:53 +0100

    zad.2

--- js/app.js
+++ js/app.js
@@ -16,6 +16,22 @@ forDropdown.addEventListener('mouseleave', function(){
 	dropdown.style.display='none';
 });
 
+//Zadanie 2
 
-    })
-
+var buttons = document.querySelectorAll('.read-more');
+ 
+ function showHide() {
+	var textArea = this.previousElementSibling;
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      };
+  };
+     // buttons[0].addEventListener('click', showHide);
+ for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+  });
